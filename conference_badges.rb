c93ed523e73a2097a2999740ed30e1a38d7164a4
Alex Wilson <fonzmp@gmail.com>
2018-05-14 20:47:19 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_array = []
+  array.each do |names|
+    current_name = badge_maker(names)
+    badge_array.push(current_name)
+  end
+  return badge_array
+end
+
+def assign_rooms(array)
+  room = 1
+  room_array = []
+  array.each do |person|
+    room_declaration = "Hello, #{person}! You'll be assigned to room #{room}!"
+    room += 1
+    room_array.push(room_declaration)
+  end
+  return room_array
+end
+
+def printer
+
+end
